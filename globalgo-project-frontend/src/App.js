0f2a6f010ee2a44d85d7c26b9810914621fe0c63
skjwise <skjwise@hotmail.com>
2020-01-08 16:27:18 +0000

    removed rails backend

--- globalgo-project-frontend/src/App.js
+++ globalgo-project-frontend/src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import MapBrowser from './containers/MapBrowser';
+import HomePage from './containers/HomePage';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Donation App</h2>
-
-      </header>
-    </div>
-  );
+const initialState = {
+  userThemes: [],
+  themes: [],
+  updateThemes: false,
+  selectedCountry: "",
+  updatedSelectedCountry: false,
+  selectedProject: {},
+  user: {}
 }
+
+class App extends Component {
+
+  state = {initialState}
+
+
+  // need to fetch data from API 
+  // also need to add user functions 
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Donation App</h2>
+
+        </header>
+      </div>
+    );
+  }
+}
+
 
 export default App;
