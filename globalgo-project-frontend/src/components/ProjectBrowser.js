175b9967febec6741fd77f544bc3f2ca8f67541c
skjwise <skjwise@hotmail.com>
2020-01-10 09:12:14 +0000

    installed react-scroll

--- globalgo-project-frontend/src/components/ProjectBrowser.js
+++ globalgo-project-frontend/src/components/ProjectBrowser.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import {Grid, Dropdown, Header, Icon, Loader} from 'semantic-ui-react';
+import '../App.css';
+import CountryCard from './CountryCard';
+import ThemesDropdown from './ThemesDropdown';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import { withRouter } from 'react-router-dom';
+import * as Scroll from 'react-scroll';
+import {Link, Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
+import config from 'react-global-configuration';
+
+// localhost3001/projects
 
 export class ProjectBrowser extends Component {
     render() {
