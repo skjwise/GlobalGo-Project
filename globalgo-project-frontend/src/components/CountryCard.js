ccea816e5185558cf24f51dcfc1a3af93f6dc166
skjwise <skjwise@hotmail.com>
2020-01-10 14:08:05 +0000

    removed Home Page, using Login Page instead

--- globalgo-project-frontend/src/components/CountryCard.js
+++ globalgo-project-frontend/src/components/CountryCard.js
@@ -1,6 +1,53 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {Loader, Visibility, Card, Icon, Image, Progress} from 'semantic-ui-react';
 
 export class CountryCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            starred: false,
+            show: false
+        }
+    }
+
+    // static PropTypes = {
+    //     src: PropTypes.string.isRequired,
+    //     size: PropTypes.string
+    // }
+
+    static defaultProps = {size: `large`}
+
+    showImage = () => {
+        this.setState({show: true})
+    }
+
+    componentDidMount(){
+        this.checkIfStarred()
+    }
+
+    handleStar = () => {
+        const userId = localStorage.userid
+        const projectId = this.props.id
+        const url = `${this.props.API_URL}/user_starred_projects`
+        const headers = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({user_id: userId, project_id: projectId})
+        }
+        fetch(url, headers)
+        .then(r => r.json())
+        .then(json => {
+            if(!json.error){
+                this.setState({starred: true})
+            }
+        })
+    }
+
+    
     render() {
         return (
             <div>
