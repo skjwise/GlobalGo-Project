5912de1646f16ddc57d86e57452f4a26cfd81d45
skjwise <skjwise@hotmail.com>
2020-01-22 09:06:41 +0000

    Education projects working

--- globalgo-project-frontend/src/components/EducationProjectCard.js
+++ globalgo-project-frontend/src/components/EducationProjectCard.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
 import { Card, Image, Button, Progress } from 'semantic-ui-react';
+import Modal from './Modal'
+import ProjectDetails from './ProjectDetails'
 
-const EducationProjectCard = ({project}) => {
+
+const EducationProjectCard = ({project, user}) => {
     const history = useHistory();
+    const [isModalOpen, setModal] = useState(false)  
+    const [singleProject, setProject] = useState([]);
+
+
 
     const handleDonateClick = () => {
         console.log(project)
         history.push("/donation");
     }
 
-    // const selectProject = project => {
-    //     setProject(singleProject);
-    //     setModal(true);
-    //     console.log('project details', project);
-    // }
+    const selectProject = project => {
+        setProject(singleProject);
+        setModal(true);
+        console.log('project details', project);
+    }
 
     const progressBar = () => {
        return Math.floor(project.funding.toFixed(0) / project.goal.toFixed(0) * 100)
@@ -36,10 +43,18 @@ const EducationProjectCard = ({project}) => {
             </Card.Description>
             <br/>
             <Button onClick={() => handleDonateClick()} > Donate </Button>
-            {/* <Button onClick={() => selectProject(project)} > More Details</Button> */}
+            <Button onClick={() => selectProject(project)} > More Details</Button>
             </Card.Content>
         </Card>
-            
+        {isModalOpen && (
+            <Modal closeModal={() => setModal(false)}>
+                <ProjectDetails
+                    project={project}
+                    user={user}
+                    closeModalOnSave={() => setModal(false)}
+                />
+            </Modal>
+        )}
         </div>
     );
 }
